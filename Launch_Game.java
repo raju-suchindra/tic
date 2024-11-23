94e94a1353af6ea0349f138ed8b944582712e5dc
Padmaraju Suchindra <126499020+raju-suchindra@users.noreply.github.com>
2024-11-23 21:43:39 +0530

    Update Launch_Game.java

--- Launch_Game.java
+++ Launch_Game.java
@@ -3,106 +3,117 @@ import java.util.Scanner;
 
 class TicTacToe
 {
-	static char[][] board;
-	
+	static char [][] board;
 	public TicTacToe()
 	{
-		board = new char[3][3];
-		initBoard();
+		board=new char[3][3];
+		initboard();
 	}
-	
-//initialize the board with single space
-	void initBoard()
+
+	void initboard()
 	{
-		for(int i = 0 ; i < board.length ; i++)
-		{
-			 for(int j = 0 ; j < board[i].length ; j++)
-			 {
-			 	board[i][j] = ' ';
-			 }
+		for(int i=0;i<board.length;i++) {
+			for(int j=0;j<board[i].length;j++)
+			{
+				board[i][j]=' ';
+			}
 		}
 	}
 
-	//	Displaying board
-	static void dispBoard()
+	static void dispboard()
 	{
-		System.out.println("----------");
-		for(int i = 0 ; i < board.length ; i++)
-		{
+		System.out.println("-------------");
+		for(int i=0;i<board.length;i++) {
 			System.out.print("| ");
-			 for(int j = 0 ; j < board[i].length ; j++)
-			 {
-			 	System.out.print(board[i][j] + "| ");
-			 }
-			 System.out.println("");
-			 System.out.println("----------");
+			for(int j=0;j<board[i].length;j++)
+			{
+				System.out.print(board[i][j]+" | ");
+			}
+			System.out.println();
+			System.out.println("-------------");
 		}
 	}
-	
-	//marking 'o' and 'x'
-	static void placeMark(int row , int col , char mark){
-		if(row >= 0 && row <= 2 && col >= 0 && col <= 2) {
-			board[row][col] = mark;
+
+	static void placemark(int row, int col,char mark)
+	{
+		if(col>=0 && col<=2 && row>=0 && row<=2)
+		{
+			board[row][col]=mark;
 		}
 		else {
-			System.out.println("Invalid Input");
+			System.out.println("Invalid input");
 		}
 	}
-	
-	static boolean checkColWin()
+
+	static boolean colwincheck()
 	{
-		for(int j = 0 ; j<=2;j++) {
-			if(board[0][j] != ' ' && board[0][j] == board[1][j] && board[1][j] == board[2][j]) {
-				return true;
-			}
-		}
-		return false;
-	}
- 
-	static boolean checkRowWin()
-	{
-		for(int i= 0 ;i<=2;i++) {
-			if(board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+		for(int j=0;j<3;j++)
+		{
+			if(board[0][j]!=' ' && board[0][j]==board[1][j] && board[1][j]==board[2][j])
+			{
 				return true;
 			}
 		}
 		return false;
 	}
 
-	static boolean checkDiagWin()
+	static boolean rowwincheck()
 	{
-		if(board[0][0]!=' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2] || board[0][2]!=' '&& board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+		for(int j=0;j<3;j++)
+		{
+			if(board[j][0]!=' ' && board[j][0]==board[j][1] && board[j][1]==board[j][2])
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	static boolean diagcheck()
+	{
+		if((board[0][0]!=' ' && board[0][0]==board[1][1] && board[1][1]==board[2][2]) || 
+				(board[0][2]!=' ' && board[0][2]==board[1][1] && board[1][1]==board[2][0]))
+		{
 			return true;
 		}
-		else {
+		else{
 			return false;
 		}
 	}
+	
+	static boolean checkDraw()
+	{
+		for(int  i = 0 ; i<=2 ;i++) {
+			for(int j = 0 ; j<=2 ;j++) {
+				if(board[i][j] == ' ') {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 }
 
-abstract class Player{
+abstract class Player
+{
 	String name;
 	char mark;
 	abstract void makeMove();
 	
 	boolean isValidMove(int row , int col)
 	{
-		if(row>=0 && row<=2 && col>=0 && col<=2)
-		{
-			if(TicTacToe.board[row][col] == ' ') {
+		if(row>=0 && row <=2 && col>=0 && col<=2) {
+			if(TicTacToe.board[row][col] ==' ') {
 				return true;
 			}
 		}
 		return false;
-		
 	}
 	
 }
-//human class
-class HumanPlayer extends Player
-{
-	
-	HumanPlayer(String name , char mark)
+class AIPlayer extends Player{
+
+	AIPlayer(String name, char mark)
 	{
 		this.name = name;
 		this.mark = mark;
@@ -111,66 +122,65 @@ class HumanPlayer extends Player
 	void makeMove()
 	{
 		Scanner scan = new Scanner(System.in);
-		int a =  scan.nextInt();
-		
-		int row,col;
+		int row , col;
 		do {
-			System.out.print("Enter the row and col");
+			Random r = new Random();
+			row = r.nextInt(3);
+			col = r.nextInt(3);		}
+		while(!isValidMove(row,col));
+		
+		TicTacToe.placemark(row, col, mark);
+	}
+
+}
+
+
+class HumanPlayer extends Player{
+
+	HumanPlayer(String name, char mark)
+	{
+		this.name = name;
+		this.mark = mark;
+	}
+	
+	void makeMove()
+	{
+		Scanner scan = new Scanner(System.in);
+		int row , col;
+		do {
+			System.out.println("Enter the row and col");
 			row = scan.nextInt();
 			col = scan.nextInt();
 		}
 		while(!isValidMove(row,col));
-
-		TicTacToe.placeMark(row , col , mark);
+		
+		TicTacToe.placemark(row, col, mark);
 	}
 
 }
 
-class AiPlayer extends Player
-{
-	AiPlayer(String name , char mark)
-	{
-		this.name = name;
-		this.mark = mark;
-	}
-	
-	void makeMove()
-	{
-		Scanner scan = new Scanner(System.in);
-		int row,col;
-		do {
-			Random r = new Random();
-			row = r.nextInt(3);
-			col = r.nextInt(3);
-		}
-		while(!isValidMove(row,col));
-
-		TicTacToe.placeMark(row , col , mark);
-	}
-	
-}
-
-// Main Class
-public class Launch_Game {
+public class LaunchGame {
 
 	public static void main(String[] args) {
-		TicTacToe t = new TicTacToe();
+		TicTacToe t=new TicTacToe();
 		
-		HumanPlayer p1 = new HumanPlayer("Suchi",'X');
-		HumanPlayer p2 = new HumanPlayer("Raju",'O');
-		// AiPlayer p2 = new AiPlayer("Tai " , 'O');
+		HumanPlayer p1 = new HumanPlayer("Bavaji" , 'X');
+		AIPlayer p2 = new AIPlayer("Tai " , 'O');
 		
 		
 		Player cp;
 		cp = p1;
-		
 		while(true) {
-			System.out.println(cp.name + "'s turn");
+
+			System.out.println(cp.name +" turn");
 			cp.makeMove();
-			TicTacToe.dispBoard();
-			
-			if(TicTacToe.checkRowWin() || TicTacToe.checkColWin() || TicTacToe.checkDiagWin()) {
+			TicTacToe.dispboard();
+			if(TicTacToe.colwincheck() || TicTacToe.diagcheck() || TicTacToe.rowwincheck()) {
 				System.out.println(cp.name + " has won the game");
+				break;
+			}
+			else if(TicTacToe.checkDraw()) {
+				System.out.println("Game is a Draw");
 				break;
 			}
 			else {
@@ -182,6 +192,7 @@ public class Launch_Game {
 				}
 			}
 		}
+		
 	}
 
 }
